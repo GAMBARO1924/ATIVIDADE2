20880c24d0aa471f5821203b569adaddce9b170e
GAMBARO1924 <149023138+GAMBARO1924@users.noreply.github.com>
2024-03-08 21:22:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
+function atividade1(){ // Não remova essa linha.
+
+  // ### Escreva Seu código aqui :) ###
 var nome = "Fatec";
 var ano = 2024;
 
 console.log(nome);
 console.log(ano);
 
-ano =  ano + 3;
+let ano =  ano + 3;
 
-console.log(ano);
+console.log(ano); 
+
+  // ### FIM do código ###
+
+} // Não remova essa linha.
+module.exports = atividade1; // Não remova essa linha.
